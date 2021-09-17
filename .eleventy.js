9e8dda8f18a4c4d0f309311dcff6e8c9d2a3cf6d
Intergalaxen <westbergalfrida@gmail.com>
2021-09-17 15:14:46 +0200

    Made a individual page template for each article

--- .eleventy.js
+++ .eleventy.js
@@ -3,8 +3,9 @@ const { DateTime } = require("luxon");
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/style.css");
   eleventyConfig.addPassthroughCopy("./src/assets");
+
   eleventyConfig.addFilter("postDate", (dateObj) => {
-    return DateTime.fromJSDate(dateObj).toLocalString(DateTime.DATE_MED);
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
 
   return {
